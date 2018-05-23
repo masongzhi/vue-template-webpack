b39ab6295ca0a993be0cc6c726acf96c458b9bcd
masongzhi <511071161@qq.com>
2018-05-23 18:41:21 +0800

    自定义模板，增加filters和vuex选项

---
+++ template/src/page/HelloWorld/store.js
@@ -0,0 +1,18 @@
+const state = {
+  data: {}
+}
+
+const actions = {
+}
+
+const mutations = {
+}
+
+const module = {
+  namespaced: true,
+  state,
+  actions,
+  mutations
+}
+
+export default module
