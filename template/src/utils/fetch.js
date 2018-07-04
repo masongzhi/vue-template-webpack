838591216b8db4037d22674dbcf33ff030790082
masongzhi <511071161@qq.com>
2018-07-04 21:09:20 +0800

    优化fetch文件

--- template/src/utils/fetch.js
+++ template/src/utils/fetch.js
@@ -1,19 +1,22 @@
-import axios from 'axios'
-import URI from 'urijs'
+import axios from 'axios';
+import URI from 'urijs';
 
-export default function fetch (url, options) {
-  const config = handleConfig(url, options)
+export default function fetch(url, options) {
+  const config = handleConfig(url, options);
 
-  return axios(config)
+  return axios(config).then(handleResponse);
 }
 
-function ensureAbsoluteUrl (input) {
-  if (typeof input !== 'string') return input
-  if (URI(input).is('absolute')) return input
-  return URI(input).normalize().toString()
+function ensureAbsoluteUrl(input) {
+  if (typeof input !== 'string') return input;
+  if (URI(input).is('absolute')) return input;
+  // return URI('/api/v1' + input)
+  return URI(input)
+    .normalize()
+    .toString();
 }
 
-function handleConfig (url, options) {
+function handleConfig(url, options) {
   const defaultOptions = {
     method: 'get',
     url: ensureAbsoluteUrl(url),
@@ -21,24 +24,28 @@ function handleConfig (url, options) {
     withCredentials: true, // 是否允许带cookie
     headers: {
       'Content-Type': 'application/json',
-      'X-Requested-With': 'XMLHttpRequest'
-    }
-  }
+      'X-Requested-With': 'XMLHttpRequest',
+    },
+  };
 
-  if (!options) return defaultOptions
+  if (!options) return defaultOptions;
 
   let finalOptions = {
     ...defaultOptions,
-    ...options
-  }
+    ...options,
+  };
 
-  finalOptions.method = String.prototype.toLowerCase.call(finalOptions.method)
+  finalOptions.method = String.prototype.toLowerCase.call(finalOptions.method);
 
   if (options && options.query) {
-    finalOptions.params = options.query
+    finalOptions.params = options.query;
   } else if (options && options.body) {
-    finalOptions.data = options.body
+    finalOptions.data = options.body;
   }
 
-  return finalOptions
+  return finalOptions;
+}
+
+export function handleResponse(response) {
+  return response.data;
 }
